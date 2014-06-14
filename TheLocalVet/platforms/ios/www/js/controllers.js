3fa7bbfff536f9c8e398918b7186854e2007e94e
Leif H. Larsen <me@leiflarsen.org>
2014-06-14 22:37:15 +0200

    Started development
    Started development and early testing of ionic.

--- TheLocalVet/platforms/ios/www/js/controllers.js
+++ TheLocalVet/platforms/ios/www/js/controllers.js
@@ -1,15 +1,10 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope) {
+.controller('MainCtrl', function($scope, $state) {
+  $scope.searchMyLocation = function () {
+    $state.go('mylocation')
+  }
 })
 
-.controller('FriendsCtrl', function($scope, Friends) {
-  $scope.friends = Friends.all();
+.controller('MyLocCtrl', function ($scope) {
 })
-
-.controller('FriendDetailCtrl', function($scope, $stateParams, Friends) {
-  $scope.friend = Friends.get($stateParams.friendId);
-})
-
-.controller('AccountCtrl', function($scope) {
-});
