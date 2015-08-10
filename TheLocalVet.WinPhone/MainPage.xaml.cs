d906ff696476fc821e00d87cdc426fbd29935d71
Leif Larsen <me@leiflarsen.org>
2015-08-10 22:05:33 +0200

    WinPhone
    Added windows phone project.

---
+++ TheLocalVet.WinPhone/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using TheLocalVet;
+using TheLocalVet.WinPhone.Resources;
+using Xamarin.Forms;
+
+namespace TheLocalVet.WinPhone
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Forms.Init();
+            Content = TheLocalVet.App.GetMainPage().ConvertPageToUIElement(this);
+        }
+    }
+}
--- TheLocalVet/TheLocalVet.cs
+++ TheLocalVet/TheLocalVet.cs
@@ -2,25 +2,48 @@
 
 using Xamarin.Forms;
 
+using TheLocalVet.ViewModels;
+
 namespace TheLocalVet
 {
 	public class App : Application
 	{
 		public App ()
 		{
-			// The root page of your application
-			MainPage = new ContentPage {
-				Content = new StackLayout {
-					VerticalOptions = LayoutOptions.Center,
-					Children = {
+            // The root page of your application
+            MainPage = new ContentPage
+            {
+                Content = new StackLayout
+                {
+                    VerticalOptions = LayoutOptions.Center,
+                    Children = {
 						new Label {
 							XAlign = TextAlignment.Center,
 							Text = "Welcome to Xamarin Forms!"
 						}
 					}
-				}
-			};
+                }
+            };			
 		}
+
+        public static Page GetMainPage()
+        {
+            return new NavigationPage(new ContentPage
+            {
+                Content = new StackLayout
+                {
+                    VerticalOptions = LayoutOptions.Center,
+                    Children =
+                    {
+                        new Label
+                        {
+                            XAlign = TextAlignment.Center,
+                            Text = "Welcome to Xamarin Forms!"
+                        }
+                    }
+                }
+            });
+        }
 
 		protected override void OnStart ()
 		{
