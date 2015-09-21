7c638d62b585db828214a2f6613a9727fb72c790
leif-larsen <me@leiflarsen.org>
2015-09-21 20:43:32 +0200

    Added hamburger menu structure. Will make the master detail pages better, so I get tabbed and hamburger menu :D

---
+++ TheLocalVet/Pages/Menu/MenuListData.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheLocalVet.Pages.Menu
+{
+    public class MenuListData : List<MenuItem>
+    {
+        public MenuListData()
+        {
+            this.Add(new MenuItem()
+            {
+                Title = "Contracts",
+                IconSource = "contracts.png",
+                //TargetType = typeof(ContractsPage)
+            });
+
+            this.Add(new MenuItem()
+            {
+                Title = "Leads",
+                IconSource = "Lead.png",
+               // TargetType = typeof(LeadsPage)
+            });
+
+            this.Add(new MenuItem()
+            {
+                Title = "Accounts",
+                IconSource = "Accounts.png",
+                //TargetType = typeof(AccountsPage)
+            });
+
+            this.Add(new MenuItem()
+            {
+                Title = "Opportunities",
+                IconSource = "Opportunity.png",
+                //TargetType = typeof(OpportunitiesPage)
+            });
+        }
+    }
+}
--- TheLocalVet/Pages/NearestVetPage.cs
+++ TheLocalVet/Pages/NearestVetPage.cs
@@ -3,21 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection.Emit;
 using System.Text;
-
+using TheLocalVet.Pages.Menu;
 using Xamarin.Forms;
 
 namespace TheLocalVet.Pages
 {
-    public class NearestVetPage : ContentPage
+    public class NearestVetPage : MasterDetailPage
     {
         public NearestVetPage()
         {
-            Content = new StackLayout
-            {
-                Children = {
-                    new Label { Text = "Hello nearest vet" }
-                }
-            };
+            var menuPage = new MenuPage();
+
+            menuPage.Menu.ItemSelected += (sender, e) => NavigateTo(e.SelectedItem as Menu.MenuItem);
+
+            Master = menuPage;
+            Detail = new NavigationPage(new EmergencyContactPage());
+        }
+
+        void NavigateTo(Menu.MenuItem menu)
+        {
+            Page displayPage = (Page)Activator.CreateInstance(menu.TargetType);
+
+            Detail = new NavigationPage(displayPage);
+
+            IsPresented = false;
         }
     }
 }
