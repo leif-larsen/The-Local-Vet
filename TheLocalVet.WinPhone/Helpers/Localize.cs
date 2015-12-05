c6a4468be6a3191e7ab70cc76fd2722322bcb32a
leif-larsen <me@leiflarsen.org>
2015-12-05 17:33:44 +0100

    Translated app and WP solution.

---
+++ TheLocalVet.WinPhone/Helpers/Localize.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Globalization;
+using TheLocalVet.Interfaces;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(TheLocalVet.WinPhone.Helpers.Localize))]
+namespace TheLocalVet.WinPhone.Helpers
+{
+    public class Localize : ILocalize
+    {
+        public CultureInfo GetCurrentCultureInfo()
+        {
+            return System.Threading.Thread.CurrentThread.CurrentUICulture;
+        }
+    }
+}
